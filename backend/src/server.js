e01e967bd83340af77263ff70fcd150635c4b30e
Logan Pritchett <logan.pritchett01@gmail.com>
2024-11-24 17:14:57 -0600

    Revert "fix: update build script for monorepo"
    This reverts commit e395fc6056d06f22c6dc686da24ea7d4663fea32.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,9 @@
 const { PORT = process.env.PORT || 5000 } = process.env;
 
 const path = require("path");
-const express = require("express");
-const app = require("./app");
-
-// Create a new router for API routes
-const apiRouter = express.Router();
-
-// Mount the dishes and orders routes on the API router
-apiRouter.use("/dishes", require("./dishes/dishes.router"));
-apiRouter.use("/orders", require("./orders/orders.router"));
-
-// Mount the API router at /api
-app.use("/api", apiRouter);
-
-// Serve static files from the React frontend app
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/build")));
-
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
-  });
-}
+const app = require(
+  path.resolve(`${process.env.SOLUTION_PATH || ""}`, "src/app")
+);
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
